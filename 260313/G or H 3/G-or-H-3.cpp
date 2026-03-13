dd968ae040f617e07d8598d278cf9e2d58823ac9
D-eepblue <103310128+D-eepblue@users.noreply.github.com>
2026-03-13 14:11:23 +0900

    Title: G or H 3, Time: 74ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260313/G or H 3/G-or-H-3.cpp
+++ 260313/G or H 3/G-or-H-3.cpp
@@ -10,15 +10,14 @@ char loc[10001];
 int main() {
     cin >> N >> K;
 
-    int end = 0, ret = 0;
+    int start = 1 << 30, end = 0, ret = 0;
 
     for (int i = 0; i < N; i++) {
         cin >> x >> c;
         loc[x] = c;
+        start = min(start, x);
         end = max(end, x);
     }
-
-    end -= K;
 
     for (int i = 1; i <= end; i++) {
         int score = 0;
