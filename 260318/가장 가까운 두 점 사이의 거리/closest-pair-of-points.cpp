a546dfee11eb8e67743840968e10e45cf20ccf35
D-eepblue <103310128+D-eepblue@users.noreply.github.com>
2026-03-18 15:15:44 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260318/가장 가까운 두 점 사이의 거리/closest-pair-of-points.cpp
+++ 260318/가장 가까운 두 점 사이의 거리/closest-pair-of-points.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int n;
 int x[100], y[100];
-int dist = 0x7fff'ffff;
 int ret = 0x7fff'ffff;
 
 int main() {
@@ -16,15 +15,11 @@ int main() {
     }
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i == j) continue;
+        for (int j = i + 1; j < n; j++) {
             int x1 = x[i], x2 = x[j];
             int y1 = y[i], y2 = y[j];
-            int d = abs(x1 - x2) + abs(y1 - y2);
-            if (dist > d) {
-                dist = d;
-                ret = pow(x1 - x2, 2) + pow(y1 - y2, 2);
-            }
+            int dist = pow(x1 - x2, 2) + pow(y1 - y2, 2);
+            ret = min(ret, dist);
         }
     }
 
