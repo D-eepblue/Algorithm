a730607c6956e0f68d9e15f60c946bfbd87145ec
D-eepblue <103310128+D-eepblue@users.noreply.github.com>
2026-03-20 13:26:27 +0900

    Title: 흥미로운 숫자 2, Time: 21ms, Memory: 0MB, Status: Passed - Codetree

--- 260320/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 260320/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -10,21 +10,25 @@ int main() {
     int cnt = 0;
 
     for (int num = X; num <= Y; num++) {
-        bool check[10] = { false, };
-        if(num >= 1000000) check[num / 1000000] = true;
-        if(num >= 100000) check[num / 100000 % 10] = true;
-        if (num >= 10000) check[num / 10000 % 10] = true;
-        if (num >= 1000) check[num / 1000 % 10] = true;
-        check[num / 100 % 10] = true;
-        check[num / 10 % 10] = true;
-        check[num / 1 % 10] = true;
+        int check[10] = { false, };
+        if(num >= 1000000) check[num / 1000000]++;
+        if(num >= 100000) check[num / 100000 % 10]++;
+        if (num >= 10000) check[num / 10000 % 10]++;
+        if (num >= 1000) check[num / 1000 % 10]++;
+        check[num / 100 % 10]++;
+        check[num / 10 % 10]++;
+        check[num / 1 % 10]++;
 
         int checkCnt = 0;
+        int minCnt = 30;
 
         for (int i = 0; i < 10; i++) {
-            if (check[i]) checkCnt++;
+            if (check[i] > 0) {
+                checkCnt++;
+                minCnt = min(minCnt, check[i]);
+            }
         }
-        if (checkCnt == 2) cnt++;
+        if (checkCnt == 2 && minCnt == 1) cnt++;
     }
 
     cout << cnt;
