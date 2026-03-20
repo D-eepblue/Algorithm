ada71736d289cd7dd710b4f16d6d5392ebcaa332
D-eepblue <103310128+D-eepblue@users.noreply.github.com>
2026-03-20 16:53:09 +0900

    Title: 등차수열, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260320/등차수열/arithmetic-sequence.cpp
+++ 260320/등차수열/arithmetic-sequence.cpp
@@ -16,10 +16,12 @@ int main() {
     int cnt = 0;
 
     for (int k = 2; k <= 99; k++) {
+        if (check[k]) continue;
+        
         int lcnt = 0;
 
         for (int i = 1; i <= k - 1; i++) {
-            if (check[i] && check[2 * k - i])lcnt++;
+            if (check[i] && check[2 * k - i]) lcnt++;
         }
 
         cnt = max(cnt, lcnt);
