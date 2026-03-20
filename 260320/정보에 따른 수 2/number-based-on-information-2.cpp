dd30e23f71d401b70a693aca53f7362b49741b45
D-eepblue <103310128+D-eepblue@users.noreply.github.com>
2026-03-20 16:08:53 +0900

    Title: 정보에 따른 수 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260320/정보에 따른 수 2/number-based-on-information-2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int T, a, b;
+int x;
+char c;
+char line[1001];
+
+int main() {
+    cin >> T >> a >> b;
+
+    for (int i = 0; i < T; i++) {
+        cin >> c >> x;
+        line[x] = c;
+    }
+    
+    int ret = 0;
+
+    for (int pos = a; pos <= b; pos++) {
+        int dS = 1001;
+        int dN = 1001;
+        // left search
+        for (int left = pos; left >= a; left--) {
+            if (line[left] == 'N') dN = min(dN, pos - left);
+            else if (line[left] == 'S') dS = min(dS, pos - left);
+        }
+
+        // right serch
+        for (int right = pos; right <= b; right++) {
+            if (line[right] == 'N') dN = min(dN, right - pos);
+            else if (line[right] == 'S') dS = min(dS, right - pos);
+        }
+        if (dS <= dN) ret++;
+    }
+
+    cout << ret;
+
+    return 0;
+}
