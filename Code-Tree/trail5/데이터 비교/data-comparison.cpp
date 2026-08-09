6450161e5a1ba112677fd312b89c5d1f5b3bb65a
D-eepblue <103310128+D-eepblue@users.noreply.github.com>
2026-08-09 12:17:06 +0900

    [Passed] 데이터 비교 (68ms, 2MB)

---
+++ Code-Tree/trail5/데이터 비교/data-comparison.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int n, m, v;
+int arr[100'000];
+unordered_set<int> uset;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> v;
+        uset.insert(v);
+    }
+
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < m; i ++) {
+        if (uset.find(arr[i]) == uset.end()) {
+            cout << "0 ";
+        }
+        else cout << "1 ";
+    }
+
+    return 0;
+}
