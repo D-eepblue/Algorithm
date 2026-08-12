99e56687eee12438a26201607317051c455b6f47
D-eepblue <103310128+D-eepblue@users.noreply.github.com>
2026-08-12 21:33:23 +0900

    [Passed] 숫자 빠르게 찾기 2 (320ms, 2MB)

---
+++ Code-Tree/trail5/숫자 빠르게 찾기 2/find-number-fast-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int n, m, v;
+set<int> mset;
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> v;
+        mset.insert(v);
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> v;
+        auto iter = mset.lower_bound(v);
+        if (iter == mset.end()) cout << -1 << "\n";
+        else cout << *iter << "\n";
+    }
+
+    return 0;
+}
